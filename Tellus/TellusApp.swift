fb21cbb72e300ddfde0d5e25298d3f2993e7b6a1
Tim Gunnarsson <tim.awg@outlook.com>
2023-09-14 00:53:26 +0200

    Initial Commit

---
+++ Tellus/TellusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TellusApp.swift
+//  Tellus
+//
+//  Created by Tim Gunnarsson on 2023-09-14.
+//
+
+import SwiftUI
+
+@main
+struct TellusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
